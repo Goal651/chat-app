fea75046db8943766e71ae674c1c722b151049c2
Wilson Goal BUGIRI <bugiriwilson651@gmail.com>
2024-12-18 01:38:58 +0200

    Update Tester.tsx

--- client/src/test/Tester.tsx
+++ client/src/test/Tester.tsx
@@ -1,12 +1,8 @@
 
-import SocketConfig from '../config/SocketConfig';
-
-
 const Tester = ({ serverUrl }: { serverUrl: string }) => {
     return (
         <div>
             <div className='text-3xl'>Testing socket</div>
-            <SocketConfig />
         </div>
     )
 
