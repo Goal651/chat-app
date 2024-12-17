a0c16a6b93bca1b7507826a4835a412da178be39
Wilson Goal BUGIRI <bugiriwilson651@gmail.com>
2024-12-18 01:40:33 +0200

    Update Dashboard.tsx

--- client/src/main/Dashboard.tsx
+++ client/src/main/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Socket } from "socket.io-client"
 import Navigator from "../content/Navigator"
 
 export default function Dashboard({ socket }: { socket: Socket }) {
+    console.log(socket)
     return (
         <div className="bg-slate-700 h-screen p-5 flex space-x-10">
             <div className="md:w-1/6 bg-blue-600 md:p-2 rounded-2xl">
